9bacf5c6ab2cba43fd3f2474213128a693a59e50
Farid Mammadov <fmammadov20348@ada.edu.az>
2026-04-08 13:29:06 +0400

    refactor: replace String queue with proper inheritance hierarchy

--- MainApp.java
+++ MainApp.java
@@ -1,11 +1,29 @@
+// MainApp.java - YENİ VERSİYA (əvvəlkini sil, bunu yaz)
 import java.util.*;
 
+abstract class MenuItem {
+    private String name;
+    public MenuItem(String name) { this.name = name; }
+    public String getName() { return name; }
+    public abstract Map<String, Integer> getIngredients();
+}
+
+class Burger extends MenuItem {
+    public Burger() { super("Burger"); }
+    public Map<String, Integer> getIngredients() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Buns", 2);
+        map.put("Meat", 1);
+        return map;
+    }
+}
+
 public class MainApp {
+    private static Queue<MenuItem> taskQueue = new LinkedList<>();
+    
     public static void main(String[] args) {
-        Queue<String> taskQueue = new LinkedList<>();
-        taskQueue.add("Burger");
-        taskQueue.add("Pizza");
-        
-        System.out.println("Task queue size: " + taskQueue.size());
+        taskQueue.add(new Burger());
+        System.out.println("Queue size: " + taskQueue.size());
+        System.out.println("Task: " + taskQueue.peek().getName());
     }
 }
